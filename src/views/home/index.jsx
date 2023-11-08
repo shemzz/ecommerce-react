7efe65ce9d715967a241c3728577b4588bcae1b7
Shemang David <davidshemang@gmail.com>
2023-11-08 15:47:44 +0100

    style banner

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -32,14 +32,14 @@ const Home = () => {
       <div className="home">
         <div className="banner">
           <div className="banner-desc">
-            <h1 className="text-thin">
-              <strong>See</strong>
-              &nbsp;everything with&nbsp;
-              <strong>Clarity</strong>
+            <h2>Style Destination</h2>
+            <h1 className="text-thin" style={{marginTop: 0}}>
+              <strong>Jewelry</strong>
+              &nbsp;Collection&nbsp;
+
             </h1>
-            <p>
-              Buying eyewear should leave you happy and good-looking, with money in your pocket.
-              Glasses, sunglasses, and contacts—we’ve got your eyes covered.
+            <p style={{fontWeight: '200'}}>
+            Discover a world of exquisite craftsmanship and timeless beauty at ChristaGold. We are your premier destination for unique jewelry, handcrafted with the finest gold and dazzling diamonds.
             </p>
             <br />
             <Link to={SHOP} className="button">
