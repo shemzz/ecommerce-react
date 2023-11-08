ac095d59398aeb1f3f3e56226fcf5696938ddcd3
Shemang David <davidshemang@gmail.com>
2023-11-08 15:07:43 +0100

    fix mobile menu

--- src/components/common/AdminSidePanel.jsx
+++ src/components/common/AdminSidePanel.jsx
@@ -1,4 +1,4 @@
-import { ADMIN_PRODUCTS } from '@/constants/routes';
+import { ADMIN_PRODUCTS, ADMIN_USERS } from '@/constants/routes';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
@@ -15,7 +15,14 @@ const SideNavigation = () => (
         </NavLink>
       </div>
       <div className="sidenavigation-item">
-        <h4 className="sidenavigation-menu my-0">Users</h4>
+
+        <NavLink
+          activeClassName="sidenavigation-menu-active"
+          className="sidenavigation-menu"
+          to={ADMIN_USERS}
+        >
+          Users
+          </NavLink>
       </div>
     </div>
   </aside>
--- src/components/common/MobileNavigation.jsx
+++ src/components/common/MobileNavigation.jsx
@@ -1,19 +1,29 @@
 import { BasketToggle } from '@/components/basket';
+import { FilterOutlined, ShoppingOutlined, MenuOutlined } from '@ant-design/icons';
+import * as ROUTE from '@/constants/routes';
+import { Drawer } from 'antd';
 import { HOME, SIGNIN } from '@/constants/routes';
 import PropType from 'prop-types';
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, {useState} from 'react';
+import {NavLink, Link, useLocation } from 'react-router-dom';
 import UserNav from '@/views/account/components/UserAvatar';
 import Badge from './Badge';
 import FiltersToggle from './FiltersToggle';
 import SearchBar from './SearchBar';
+import logo from '@/images/favicon.png';
 
 const Navigation = (props) => {
+  const [openMenu, setOpenMenu] = useState(false);
   const {
     isAuthenticating, basketLength, disabledPaths, user
   } = props;
   const { pathname } = useLocation();
-
+  const showMenu = () => {
+    setOpenMenu(true)
+  }
+  const onClose = () => {
+    setOpenMenu(false)
+  }
   const onClickLink = (e) => {
     if (isAuthenticating) e.preventDefault();
   };
@@ -22,9 +32,10 @@ const Navigation = (props) => {
     <nav className="mobile-navigation">
       <div className="mobile-navigation-main">
         <div className="mobile-navigation-logo">
-          <Link onClick={onClickLink} to={HOME}>
-            <h2>CHRISTAGOLD</h2>
-          </Link>
+        <div className="logo mobile">
+            <Link onClick={onClickLink} to={HOME}><img alt="Logo" src={logo} /></Link>
+            <h4>ChristaGold</h4>
+      </div>
         </div>
 
         <BasketToggle>
@@ -37,7 +48,8 @@ const Navigation = (props) => {
             >
 
               <Badge count={basketLength}>
-                <i className="fa fa-shopping-bag" style={{ fontSize: '2rem' }} />
+                <i className="fa fa-shopping-bag" style={{ fontSize: '2rem', color: "red" }} />
+                <ShoppingOutlined style={{ fontSize: '2.4rem' }} />
               </Badge>
             </button>
           )}
@@ -62,15 +74,38 @@ const Navigation = (props) => {
               )}
             </>
           )}
+          <li className='mobile-navigation-item'>
+            <MenuOutlined onClick={showMenu} style={{ fontSize: "2.4rem" }} />
+          </li>
         </ul>
       </div>
       <div className="mobile-navigation-sec">
-        <SearchBar />
-        <FiltersToggle>
+        {/* <SearchBar /> */}
+        {/* <FiltersToggle>
           <button className="button-link button-small" type="button">
             <i className="fa fa-filter" />
           </button>
-        </FiltersToggle>
+        </FiltersToggle> */}
+
+        <Drawer
+          title="Navigation"
+          placement='left'
+          onClose={onClose}
+          open={openMenu}
+          key='left'
+        >
+          <ul className="mobile-menu">
+            {/* <li>
+              <div>
+              <SearchBar /> 
+            </div>
+            </li> */}
+            <li onClick={onClose}><NavLink activeClassName="navigation-menu-active" exact to={ROUTE.HOME}>Home</NavLink></li>
+        <li onClick={onClose}><NavLink activeClassName="navigation-menu-active" to={ROUTE.SHOP}>Shop</NavLink></li>
+        <li onClick={onClose}><NavLink activeClassName="navigation-menu-active" to={ROUTE.FEATURED_PRODUCTS}>Featured</NavLink></li>
+        <li onClick={onClose}><NavLink activeClassName="navigation-menu-active" to={ROUTE.RECOMMENDED_PRODUCTS}>Recommended</NavLink></li>
+      </ul>
+          </Drawer>
       </div>
     </nav>
   );
